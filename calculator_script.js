2fbec8acf352417d3637b23561cc99389a052ca6
ajaysaini3078 <ajay.saini3078@gmail.com>
2024-02-02 17:24:29 +0530

    add some java script

--- calculator_script.js
+++ calculator_script.js
@@ -1,3 +1,10 @@
-function Click(e){
-    console.log("hello");
-}
+// function Click(e){
+//     console.log("hello");
+// }
+
+const Value1 = document.getElementById('keypad_field');
+Value1.addEventListener('click', function(e){
+    val = 
+
+    console.log(val);
+})
